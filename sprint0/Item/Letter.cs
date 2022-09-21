9acfb1aa807a6509976943a5c3b63477a41e3b3f
hamdo <hamdo@HAMDAN_LAPTOP>
2022-09-21 16:22:03 -0400

    Add project files.

---
+++ sprint0/Item/Letter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Item
+{
+    internal class Letter : ICommand
+    {
+        private Game1 game;
+        public Letter(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            if (this.game.player is Sprite)
+            {
+                Sprite item = (Sprite)this.game.item;
+                item.SourceRectangle = ItemRectangle.Letter;
+                item.Frame = 0;
+                item.Velocity = new Vector2(0, 0);
+            }
+        }
+    }
+}
+
